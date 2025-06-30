55e84dbedebf25e942875b4e1a0a661f0a065416
wamin suwito <waminsuwito1@gmail.com>
2025-06-30 19:27:44 +0000

    apakah aplikasi ini bisa dibuild ke android

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#42A5F5" />
       </head>
       <body className="font-body antialiased bg-background">
         <AppDataProvider>
