2a36f1c97490596107920d7effef2a6a50b42bdc
wamin suwito <waminsuwito1@gmail.com>
2025-07-01 04:22:16 +0000

    ini yang muncul sekarang

---
+++ src/next.config.js
@@ -0,0 +1,18 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+module.exports = nextConfig;
