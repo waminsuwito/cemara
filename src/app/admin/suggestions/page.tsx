74946c7809c6caf53f27ad6414101ff2b08f3858
wamin suwito <waminsuwito1@gmail.com>
2025-07-04 10:31:04 +0000

    saya minta dibawah "usulan/saran sopir" dikasih logo yang sama seperti p

--- src/app/admin/suggestions/page.tsx
+++ src/app/admin/suggestions/page.tsx
@@ -27,8 +27,13 @@ export default function SuggestionsPage() {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Usulan dan Saran dari Sopir</CardTitle>
-        <CardDescription>
+        <CardTitle className="text-center">Usulan dan Saran dari Sopir</CardTitle>
+        <img
+          src="https://i.ibb.co/V0NgdX7z/images.jpg"
+          alt="Logo PT Farika Riau Perkasa"
+          className="h-24 w-24 object-contain my-4 mx-auto"
+        />
+        <CardDescription className="text-center">
           Daftar usulan dan saran yang dikirimkan oleh sopir/operator untuk perbaikan.
         </CardDescription>
       </CardHeader>
