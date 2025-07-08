a1542176de869cae5efba5cc9844ac6d476b561b
wamin suwito <waminsuwito1@gmail.com>
2025-07-08 04:12:25 +0000

    Kenapa upload foto dari oprator saat ada kerusakan tidak bisa terkirim d

--- next.config.js
+++ next.config.js
@@ -10,6 +10,12 @@ const nextConfig = {
         hostname: 'placehold.co',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
